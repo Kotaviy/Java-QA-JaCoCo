09566752a114d340944e83100810bcac90bd98f9
Kseniya Radiseva <Kotaviy@gmail.com>
2023-06-19 19:58:24 +0500

    Added second test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -16,4 +16,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 34, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+        long expected = 34;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
